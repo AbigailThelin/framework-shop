69e30c800a56aa79dd35e399e2dcb491a8ed8bf4
AbigailThelin <abbyjthelin@gmail.com>
2017-08-03 15:00:39 -0600

    DONE

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
 import React from "react";
 
 import "./App.css";
-
+import router from '../router'
 import Nav from "./Nav/Nav";
 
 export function App( { children } ) {
 	return (
 		<div className="app">
 			<Nav />
+			{router}
 		</div>
 	);
 }
 
 export default App;
+
+
+// import React from "react";
+
+// import "./App.css";
+
+// import Nav from "./Nav/Nav";
+// import router from '../router';
+
+// export function App( { children } ) {
+//   return (
+//     <div className="app">
+//       <Nav />
+//       { router }
+//     </div>
+//   );
+// }
+
+// export default App;
